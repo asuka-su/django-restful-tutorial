a86f19811a9dd3034c3ce89a8654f100089463c2
susu <drizzle_0505@outlook.com>
2023-07-19 23:43:58 +0800

    Part IV

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from rest_framework import generics
+from snippets.serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics, permissions
+from .permissions import IsOwnerOrReadOnly
 
 """
 List all snippets, or create a new snippet.
@@ -8,6 +10,12 @@ List all snippets, or create a new snippet.
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # override a perform_create() method allow us to modify how the instance is saved
+    # 可以被视作在调用本有的save方法后，又调用了perform_create()方法作补充
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 """
@@ -16,6 +24,8 @@ Retrieve, update or delete a snippet instance.
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, 
+                          IsOwnerOrReadOnly]
 
 '''
 使用了rest_framework中预先集成的形式，使用方便，但是局限性相对较大。
@@ -23,3 +33,12 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 再上一层就是使用各种函数做到目标效果，会有比较多的重复代码。
 Response也是一个好用的特性！
 '''
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
