1cff02330b2fe760e631f894336e5f26805a7465
susu <drizzle_0505@outlook.com>
2023-07-20 11:27:33 +0800

    Part V

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -2,16 +2,27 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
-class SnippetSerializer(serializers.ModelSerializer):
+'''
+The HyperlinkedModelSerializer has the following differences from ModelSerializer:
+
+It does not include the id field by default.
+It includes a url field, using HyperlinkedIdentityField.
+Relationships use HyperlinkedRelatedField, instead of PrimaryKeyRelatedField.
+'''
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
-    
-    owner = serializers.ReadOnlyField(source='owner.username')
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,11 +3,22 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
+'''
+If we're going to have a hyperlinked API, we need to make sure we name our URL patterns. Let's take a look at which URL patterns we need to name.
+
+The root of our API refers to 'user-list' and 'snippet-list'.
+Our snippet serializer includes a field that refers to 'snippet-highlight'.
+Our user serializer includes a field that refers to 'snippet-detail'.
+Our snippet and user serializers include 'url' fields that by default will refer to '{model_name}-detail', which in this case will be 'snippet-detail' and 'user-detail'.
+'''
+
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()), 
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()), 
-    path('users/', views.UserList.as_view()), 
-    path('users/<int:pk>/', views.UserDetail.as_view()), 
+    path('', views.api_root), 
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'), 
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'), 
+    path('users/', views.UserList.as_view(), name='user-list'), 
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'), 
+    path('snippets/<int:pk>/hightlight/', views.SnippetHighlight.as_view(), name='snippet-highlight')
 ]
 
 urlpatters = format_suffix_patterns(urlpatterns)
